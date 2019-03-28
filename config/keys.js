4aae5bdc63dce566d13b334ff1283ecf2498ff73
andrey.grushevskiy <grushevskiy@gmail.com>
2019-03-28 13:10:15 +0000

    Connect Mongoose & MongoDB.

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+//a way of making this object available outside its file
+module.exports = {
+  mongoURI:
+    "mongodb+srv://andrey:andrey@cluster0-1vjnh.mongodb.net/test?retryWrites=true"
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("Mongo DB Connected"))
+  .catch(err => console.log(err));
 
 //basic route
 app.get("/", (req, res) => res.send("Hello world"));
