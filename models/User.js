dd7171ab13c36aa0de534e6f4f2e67a553f01da9
andrey.grushevskiy <grushevskiy@gmail.com>
2019-03-28 15:07:33 +0000

    Create user model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//create schema, what is our users collection going to have?
+const UserSchema = new Schema({
+  //define the name field
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+//export it to a variable User........(name we want to use, the actual schema)
+module.exports = User = mongoose.model("users", User);
