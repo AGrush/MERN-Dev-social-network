27e7276728767f60937efc49542e71ff37dac1ad
andrey.grushevskiy <grushevskiy@gmail.com>
2019-04-01 21:56:27 +0100

    Create routes & validation files for: posting new experience, posting new education, deleting profile&user at same time

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const passport = require("passport");
 
 // Load Validation
 const validateProfileInput = require("../../validation/profile");
+const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 //Load profile model
 const Profile = require("../../models/Profile");
@@ -177,5 +179,143 @@ router.post(
     });
   }
 );
+
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    //we wanna find by the logged in user, and the req.user.id comes from the token
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to the front of exp array
+      profile.experience.unshift(newExp);
+
+      //save gives a promise, which returns the saved profile which we respond with
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to edu array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+// when the experience created it gets an id that we can see in postman.
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // create a variable removeIndex which is the index of the experience item we want to delete
+        const removeIndex = profile.experience
+          //take the item and map it to an array of item.ids
+          .map(item => item.id)
+          //get the index of the item that matches the url :exp_id variable
+          .indexOf(req.params.exp_id);
+
+        // Splice out of array the experience item at removeIndex
+        profile.experience.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //mongoose method.
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
+
 //we have to export the router for server.js to pick it up
 module.exports = router;
---
+++ validation/experience.js
@@ -0,0 +1,28 @@
+const Validator = require("validator");
+const isEmptyAG = require("./is-empty");
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {};
+
+  //turns the undefined or null into an empty string so that we can use the inbuilt mongoose validator isEmpty whic only takes string values.
+  data.title = !isEmptyAG(data.title) ? data.title : "";
+  data.company = !isEmptyAG(data.company) ? data.company : "";
+  data.from = !isEmptyAG(data.from) ? data.from : "";
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = "Job title field is required";
+  }
+
+  if (Validator.isEmpty(data.company)) {
+    errors.company = "Company field is required";
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "From date field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmptyAG(errors)
+  };
+};
